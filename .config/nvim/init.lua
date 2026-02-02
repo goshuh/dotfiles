856b8583d74a588eac283a793d953db3691c792b
Yuanlong Li <yuanlong.li@epfl.ch>
2026-02-03 00:42:51 +0100

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -578,7 +578,7 @@ require('lazy').setup({
 
   { 'nvim-treesitter/nvim-treesitter',
     config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
+      require('nvim-treesitter.config').setup(opts)
     end,
     opts = {
       indent    = { enable = true },
